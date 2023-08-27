2993b0ad7d39576f47bab4db58d9f93b4ce55a6f
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-27 15:40:00 +0300

    window minimize

---
+++ src/test/java/com/myComp/tests/WindowMinimize.java
@@ -0,0 +1,29 @@
+package com.myComp.tests;
+
+import com.myComp.utilities.BrowserUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WindowMinimize {
+
+
+    @Test
+    public void window_minimize() {
+
+        //We have to set up WebDriverManager with Selenium 3 version
+        //WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        BrowserUtils.sleep(3);
+
+        driver.manage().window().maximize();
+
+        BrowserUtils.sleep(3);
+
+        driver.manage().window().minimize();
+
+
+    }
+}
