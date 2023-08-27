7bc771eb1b69ac48b80a4303c02e52e2eee8fa71
Erkan <ErkanOzulu@users.noreply.github.com>
2023-08-27 15:12:26 +0300

    Initial commit

---
+++ src/test/java/com/myComp/utilities/BrowserUtils.java
@@ -0,0 +1,87 @@
+package com.myComp.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.util.Set;
+
+/*
+In this class only general utility methods that are NOT related to some specific page.
+ */
+public class BrowserUtils {
+    /*
+    This method will accept int (in seconds) and execute Thread.sleep
+    for given duration
+     */
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    /*
+    - When method is called, it should switch window and verify title.
+
+        Method info:
+    • Name: switchWindowAndVerify
+    • Return type: void
+
+     This method accepts 3 arguments.
+    • Arg1: WebDriver
+    • Arg2: String expectedInUrl : for verify if the url contains given String.
+        -If conditions matches, will break loop
+    • Arg3: String expectedTitle : to be compared against actualTitle
+
+     */
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInURL,String expectedInTitle ){
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+            System.out.println("Current Url(): " + driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains(expectedInURL)){
+                break;
+            }
+        }
+
+        //5. Assert: Title contains “expectedInTitle”
+
+        String actualTitle=driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+
+
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+
+     */
+
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+    }
+
+    public static void waitForInvisibtyOf(WebElement webElement){
+
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/myComp/utilities/WebDriverFactory.java
@@ -0,0 +1,62 @@
+package com.myComp.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    // TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+    public static WebDriver getDriver(String browserType) {
+        if (browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+        } else {
+            System.out.println("Given browser type doesn't exist/or is not currently supported");
+            System.out.println("Driver=null");
+            return null;
+        }
+
+    }
+/*
+    public static WebDriver getDriver1(String browserName){
+
+        WebDriver driver;
+        switch (browserName.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.chromedriver().setup();
+                driver=new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Unknown browser type "+browserName);
+                driver=null;
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        return driver;
+    }
+
+
+ */
+
+}
